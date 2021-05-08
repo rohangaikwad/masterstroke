78f4885f50830932ef60cc2f5c42b94e770da43d
rohangaikwad <gaikwadvrohan@gmail.com>
2021-05-08 11:07:31 +0530

    Updates

--- js/main.js
+++ js/main.js
@@ -2,16 +2,42 @@ let letters = [];
 let activeLetter = 0;
 let count = 0;
 
-fetch('/data/hiragana.json')
-    .then(data => data.json())
-    .then(data => {
-        let d = data.filter(l => l.name.toLowerCase().indexOf('small') == -1);
-        letters = d;
-        count = d.length;
-        drawLetter();
-        console.log(d);
-    })
-    .catch(err => console.log(err));
+var font1 = new FontFaceObserver('Noto Sans', { weight: 400 });
+var font2 = new FontFaceObserver('Noto Sans JP', { weight: 100 });
+var font3 = new FontFaceObserver('Noto Sans JP', { weight: 300 });
+var font4 = new FontFaceObserver('Noto Sans JP', { weight: 400 });
+var font5 = new FontFaceObserver('Noto Sans JP', { weight: 500 });
+var font6 = new FontFaceObserver('Noto Sans JP', { weight: 700 });
+var font7 = new FontFaceObserver('Noto Sans JP', { weight: 900 });
+
+Promise.all([
+    font1.load('a'), font2.load('あ'), font3.load('あ'), font4.load('あ'), font5.load('あ'), font6.load('あ'), font7.load('あ')
+]).then(function () {
+    console.log('Font is available');
+    fetch('/data/hiragana.json')
+        .then(data => data.json())
+        .then(data => {
+            let d = data.filter(l => l.name.toLowerCase().indexOf('small') == -1);
+            letters = d;
+            count = d.length;
+            drawLetter();
+            console.log(d);
+        })
+        .catch(err => console.log(err));
+});
+
+
+// font1.load('a').then(() => {
+//     font2.load('あ').then(() => {
+//         font3.load('あ').then(() => {
+            
+//         });
+//     })
+// })
+
+
+
+
 
 
 let x = 3;
@@ -46,6 +72,14 @@ boxPlus.addEventListener('click', () => {
     drawLetter();
 })
 
+let fontWeight = 700;
+weights.querySelectorAll('button').forEach(btn => {
+    btn.addEventListener('click', (evt) => {
+        fontWeight = evt.target.innerText;
+        drawLetter();
+    })
+})
+
 
 let drawLetter = () => {
     let letter = letters[activeLetter];
@@ -66,7 +100,7 @@ let drawLetter = () => {
 
     canvas.add(new fabric.Text(letter.char, {
         selectable: false, eventable: false,
-        fontWeight: 'normal', textAlign: 'left', fontFamily: 'Noto Sans',
+        fontWeight: 'normal', textAlign: 'left', fontFamily: 'Noto Sans JP',
         top: 0.005 * w + 0.1 * w, left: 0.05 * w + 0.1 * w, originX: 'center', originY: 'center', fontSize: 100
     }));
 
@@ -104,13 +138,13 @@ let drawLetter = () => {
 
     let letterBoxContainerH = 1.15 * w;
     let letterBoxContainerW = 0.9 * w;
-    let boxCountV = Math.floor((letterBoxContainerH*boxCountH)/letterBoxContainerW);
+    let boxCountV = Math.floor((letterBoxContainerH * boxCountH) / letterBoxContainerW);
 
     // vertical lines
     for (let ii = 0; ii <= boxCountH; ii++) {
         canvas.add(new fabric.Rect({
             selectable: false, eventable: false,
-            left: 0.05 * w + (letterBoxContainerW/boxCountH * ii),
+            left: 0.05 * w + (letterBoxContainerW / boxCountH * ii),
             top: w * 0.2,
             width: 3,
             height: 1.15 * w,
@@ -123,7 +157,7 @@ let drawLetter = () => {
         canvas.add(new fabric.Rect({
             selectable: false, eventable: false,
             left: 0.05 * w,
-            top: w * 0.2 + (letterBoxContainerH/boxCountV * ii),
+            top: w * 0.2 + (letterBoxContainerH / boxCountV * ii),
             width: 0.9 * w,
             height: 3,
             fill: '#ccc'
@@ -136,16 +170,16 @@ let drawLetter = () => {
         for (let jj = 0; jj < boxCountH; jj++) {
             canvas.add(new fabric.Text(letter.char, {
                 selectable: false, eventable: false,
-                fontWeight: 'normal', textAlign: 'left', fontFamily: 'Noto Sans',
-                left: 0.05 * w + (letterBoxContainerW/boxCountH * jj) + (letterBoxContainerW/boxCountH)/2, opacity: 0.1, fontWeight: '700',
-                top: w * 0.2 + (letterBoxContainerH/boxCountV * ii) + (letterBoxContainerH/boxCountV)/2, originX: 'center', originY: 'center', fontSize: 700/boxCountH
+                fontWeight: 'normal', textAlign: 'left', fontFamily: 'Noto Sans JP',
+                left: 0.05 * w + (letterBoxContainerW / boxCountH * jj) + (letterBoxContainerW / boxCountH) / 2, opacity: 0.1, fontWeight: fontWeight,
+                top: w * 0.2 + (letterBoxContainerH / boxCountV * ii) + (letterBoxContainerH / boxCountV) / 2, originX: 'center', originY: 'center', fontSize: 700 / boxCountH
             }));
-        }        
+        }
     }
 }
 
 down.addEventListener('click', () => {
-    canvas.getElement().toBlob(function(blob) {
+    canvas.getElement().toBlob(function (blob) {
         saveAs(blob, letters[activeLetter].romanization + '.jpg');
     });
 });
