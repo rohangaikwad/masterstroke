ed3cea2e916aabd5c46c0313736de975670b8308
Rohan Gaikwad <gaikwadvrohan@gmail.com>
2022-06-24 20:08:24 +0530

    play sounds

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -5,6 +5,7 @@ import HiraganaList from "./../configs/hiragana";
 const Component = () => {
     const { setActiveChar, setCharListVisibility} = useContext(CommonContext);
     const [hiraganaChars, setHiraganaChars] = useState([])
+    const [playSound, setPlaySound] = useState(false);
 
     useEffect(() => {
         let chars = HiraganaList.filter(x => x.name.toLowerCase().indexOf("small") === -1).filter(x => x.romanization !== "");
@@ -12,21 +13,28 @@ const Component = () => {
         //console.log(chars)
     }, [])
 
-    return <div>
+    return <div id="charlist-container">
         <ul id="char-list">
-            {hiraganaChars.map((c,k) => <li onClick={() => {
-                setActiveChar(k);
-                setCharListVisibility(false)
+            {hiraganaChars.map((c,k) => <li className={playSound ? "sound": ""} onClick={() => {
+                if(playSound) document.getElementById(`audio_${c.hex}`).play();
+                else {
+                    setActiveChar(k);
+                    setCharListVisibility(false)
+                }
             }} key={k}>
                 {c.char !== null 
                     ? <>
                         <span className="jp">{c.char}</span>
-                        <span className="eng">{c.romanization}</span>
+                        {!playSound && <span className="eng">{c.romanization}</span>}
+                        <audio id={`audio_${c.hex}`} src={`/mp3/${c.romanization}.mp3`}/>
                     </>
                     : <></>
                 }
             </li>)}
         </ul>
+        {playSound 
+            ? <button onClick={() => setPlaySound(false)}>Sound: On</button> 
+            : <button onClick={() => setPlaySound(true)}>Sound: Off</button>}
     </div>
 }
 
